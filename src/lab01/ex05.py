b4ee7efed708d2cda09364c84301f84254819ff9
Max <nithralll@gmail.com>
2026-09-23 15:28:32 +0300

    Точка

--- src/lab01/ex05.py
+++ src/lab01/ex05.py
@@ -4,7 +4,7 @@ full_name = str.split()
 surname = full_name[0]
 name = full_name[1]
 father_name = full_name[2]
-print(f"Инициалы: {surname[0]}{name[0]}{father_name[0]}")
+print(f"Инициалы: {surname[0]}{name[0]}{father_name[0]}.")
 new_str =" ".join(full_name)
 print(f"Длина (символов): {len(new_str)}")
 
